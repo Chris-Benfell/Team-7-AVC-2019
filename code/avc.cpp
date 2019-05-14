d54da6ed165b9f57dcb599219ce6637726f055ca
Cocao <47886618+Cocao@users.noreply.github.com>
2019-05-14 14:56:07 +1200

    "Finished" quadrant 2 code
    Hopefully it can follow a line. We'll have to test it to see how it does and tune it a bit.

--- code/avc.cpp
+++ code/avc.cpp
@@ -1,96 +1,191 @@
 #include <iostream>
+#include <algorithm>
 #include <time.h>
-#include "LibE101/E101.h"
+#include "E101.h"
 #include "avc.h"
 using namespace std;
 
-// Initialize AVC to run the specified quadrant
+// Initialize AVC
 AVC::AVC(int q) {
     quadrant = q;
+    init(0);
+    hardware_exchange();
 }
 
 void AVC::openGate() {
-    // set quadrant to 2 once gate is open
+    quadrant = 1;
+    // send message??
+    while (quadrant == 1) {
+        // wait for message??
+        // set quadrant to 2 once gate is open
+    }
 }
 
 void AVC::followLine() {
     while (quadrant == 2) {
         take_picture();
-        if (checkRed()) {
-            quadrant = 4;
-        } else {
-            getBlackPx();
-            calcError();
 
-            // calculate error
-            // get change in time
-            // calculate kd
-            // calculate adjustment
-            // set motors
-            // ???
+        // Check if picture contains significant red indicating beginning of quadrant 4
+        if (checkRed()) { // Significantly red
+            quadrant = 4;
+        } else { // Line following code
+
+            // Turn image into an array of 1s (black) and 0s (white)
+            getBlackPx();
+
+            // Check if blackPx contains 1s to indicate line detected. No 1s indicates line has been lost
+            if (find(begin(blackPx), end(blackPx), 1) != end(blackPx)) { // Found line
+
+                // Calculate the error value
+                calcError();
+
+                // Check if error is equal to 0
+                if (error != 0) { // Not 0
+
+                    // Calculate motor adjustment
+                    adjustment = (kp * error) + (kd * (error - errorPrev) / (time.tv_sec - timePrev.tv_sec));
+
+                    // Set motors
+                    setMotors("turn");
+
+                } else { // error is equal to 0
+
+                    // Go straight
+                    setMotors("forward");
+                }
+            } else { // Line lost
+
+                // Reverse until line is found
+                setMotors("reverse");
+            }
         }
     }
 }
 
 void AVC::findDuck() {
-    // look for a red duck, green duck and blue duck
-    // set Quadrant to 5 when done
+    while (quadrant == 4) {
+        // look for a red duck, green duck and blue duck
+        // set Quadrant to 5 when done
+
+    }
+    stoph(); // I think this is important?
 }
 
 // Check for red spot in middle of camera indicating end of quadrant 3 and start of quadrant 4
 bool AVC::checkRed() {
+    // Record the number of green pixels
     int numRedPx = 0;
+
+    // Loop through all pixels in the image
     for (int col = CAMERAWIDTH/4; col < CAMERAWIDTH*3/4; col++) {
+
+        // Get RGB values of the pixel
         int red = get_pixel(SCANNEDROW, col, 0);
         int green = get_pixel(SCANNEDROW, col, 1);
         int blue = get_pixel(SCANNEDROW, col, 2);
 
-        if ((2.0 * red)  / (green + blue) > 2.5) {
+        // Compare ratio of R:G+B to threshold to determine if red
+        if ((2.0 * red)  / (green + blue) > 2.5) { // Is red
+            // Record red pixel
             numRedPx += 1;
         }
     }
-    return 2.0 * numRedPx / CAMERAWIDTH > 0.7;
+    // Return proportion of image that is red. 0.6 indicates a 60%
+    // red image which is likely to indicate a red spot
+    return 2.0 * numRedPx / CAMERAWIDTH > 0.6;
 }
 
+// Get array of black pixels (1s and 0s)
 void AVC::getBlackPx() {
+    // Get threshold to determine whether a pixels is black or white
     double threshold = calcThreshold();
+
+    // Loop through all pixels in the image
     for (int col = 0; col < CAMERAWIDTH; col++) {
+
+        // Get pixels whiteness
         int whiteness = get_pixel(SCANNEDROW, col, 3);
 
-        if (whiteness < threshold) {
+        // Check if black or white
+        if (whiteness < threshold) { // Is black
+            // Set pixel as black in array
             blackPx[col] = 1;
-        } else {
+        } else { // Is white
+            // Set pixel as white in the array
             blackPx[col] = 0;
         }
     }
 }
 
+// Calculate threshold to determine if a pixel is black or white
 double AVC::calcThreshold() {
+    // Set maximum and minimum possible values
     int min = INT_MAX;
     int max = INT_MIN;
+
+    // Loop through all pixels in the image
     for (int col = 0; col < CAMERAWIDTH; col++) {
+
+        // Get pixels whiteness
         int whiteness = get_pixel(SCANNEDROW, col, 3);
-        if (whiteness < min) {
+
+        // Check if lower the current min
+        if (whiteness < min) { // Is lower
+            // Set as new min
             min = whiteness;
         }
-        if (whiteness > max) {
+        // Check if higher than current max
+        if (whiteness > max) { // is higher
+            // Set as new max
             max = whiteness;
         }
     }
+    // Return average of the min and max. This is threshold.
     return (min+max)/2.0;
 }
 
+// Calculate the error value and record the time
 void AVC::calcError() {
-    lastError = error; // record last measurement
+    errorPrev = error; // Record last error measurement
+    timePrev = time; // Record last time measurement
+
+    // Loop through array of black pixels
     for (int i = 0; i < CAMERAWIDTH; i++) {
+
+        // Weight the pixels distance from the middle
         error += blackPx[i] * (i - MIDDLECOL);
     }
+
+    // measure current time to measure dt later on
+    clock_gettime(CLOCK_MONOTONIC, &time);
 }
 
-void AVC::setMotors(int left, int right) {
-    vLeft = left;
-    vRight = right;
+// Set the speed of each motor to the given value
+void AVC::setMotors(string direction) {
+
+    // Set appropriate speed values
+    if (direction == "forward") {
+        vLeft = LEFTDEFAULT;
+        vRight = RIGHTDEFAULT;
+    } else if (direction == "reverse") {
+        vLeft = RIGHTDEFAULT;
+        vRight = LEFTDEFAULT;
+    } else if (direction == "turn") {
+        vLeft = LEFTDEFAULT + adjustment;
+        vRight = RIGHTDEFAULT + adjustment;
+    }
+
+    // Set Motors speed
     set_motors(LEFTMOTOR, vLeft);
     set_motors(RIGHTMOTOR, vRight);
+
+    // update hardware
     hardware_exchange();
+}
+
+// debug function Run this to print out messages instead of cout<<""<<endl;
+void AVC::debug(string string) {
+    if (DEBUG == true) {
+        cout<<string<<endl;
+    }
 }
--- code/robot.cpp
+++ code/robot.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <time.h>
-#include "LibE101/E101.h"
+#include "E101.h"
 #include "avc.h"
 using namespace std;
 
@@ -10,12 +10,17 @@ using namespace std;
 // once line following is done (Found red spot)
 // Tilt camera and look for red/green/blue duck/cylinder (Q4)
 int main() {
-    init(0);
-    hardware_exchange();
+    AVC robot(0);
 
-    AVC robot(1);
-
-    robot.openGate();
-    robot.followLine();
-    robot.findDuck();
+    if (robot.quadrant == 1) {
+        robot.openGate();
+    } else if (robot.quadrant == 2) {
+        robot.followLine();
+    } else if (robot.quadrant == 4) {
+        robot.findDuck();
+    } else {
+        robot.openGate();
+        robot.followLine();
+        robot.findDuck();
+    }
 }
