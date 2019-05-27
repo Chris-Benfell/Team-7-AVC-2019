30825c4d87fd7adff09c3d8de9edb92c154e7539
djbennyd69 <48536262+djbennyd69@users.noreply.github.com>
2019-05-28 10:25:17 +1200

    Changed left turn speed and 90 turn detection

--- code/avc.cpp
+++ code/avc.cpp
@@ -72,7 +72,7 @@ void AVC::followLine() {
                 calcError();
 
                 // Check error values for in front of robot, to left, and to right of robot
-                if (quadrant == 3 && direction - 1 > 0 && errorLeft > -400 && errorLeft < 200 && errorLeft != 0) { // Check for a line on the left side (Q3)
+                if (quadrant == 3 && direction - 1 > 0 && errorLeft > -400 && errorLeft < -100 && errorLeft != 0) { // Check for a line on the left side (Q3)
                     // Turn 90 degrees left
                     setMotors("90 left");
 
@@ -83,7 +83,7 @@ void AVC::followLine() {
                     debug(to_string(direction));
                     sleep1(3000);
 
-                } else if (quadrant == 3 && direction + 1 < 4 && errorRight > 200 && errorRight < 400 && errorRight != 0) { // Check for a line on the right side (Q3)
+                } else if (quadrant == 3 && direction + 1 < 4 && errorRight > 100 && errorRight < 400 && errorRight != 0) { // Check for a line on the right side (Q3)
                     // Turn 90 degrees right
                     setMotors("90 right");
 
@@ -439,7 +439,7 @@ void AVC::setMotors(string direction) {
         vLeft = LEFTDEFAULT + adjustment;
         vRight = RIGHTDEFAULT + adjustment;
     } else if (direction == "90 right") { // 90 degree right turn
-        vLeft = LEFTDEFAULT;
+        vLeft = LEFTDEFAULT + 2;
         vRight = STOP;
     } else if (direction == "90 left") { // 90 degree left turn
         vLeft = STOP;
