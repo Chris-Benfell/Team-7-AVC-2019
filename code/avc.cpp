e1ae40edd08e19842fb94152b606f9976021f037
djbennyd69 <48536262+djbennyd69@users.noreply.github.com>
2019-05-28 12:20:13 +1200

    made right turns later

--- code/avc.cpp
+++ code/avc.cpp
@@ -76,7 +76,7 @@ void AVC::followLine() {
                 calcError();
 
                 // Check error values for in front of robot, to left, and to right of robot
-                if (quadrant == 3 && direction - 1 > 0 && errorLeft > 0 && errorLeft < 500 && errorLeft != 0) { // Check for a line on the left side (Q3)
+                if (quadrant == 3 && direction - 1 > 0 && errorLeft > 100 && errorLeft < 600 && errorLeft != 0) { // Check for a line on the left side (Q3)
                     // Turn 90 degrees left
                     setMotors("90 left");
 
@@ -88,7 +88,7 @@ void AVC::followLine() {
                     debug(to_string(direction));
                     sleep1(2500);
 
-                } else if (quadrant == 3 && direction + 1 < 4 && errorRight > 0 && errorRight < 500 && errorRight != 0) { // Check for a line on the right side (Q3)
+                } else if (quadrant == 3 && direction + 1 < 4 && errorRight > 100 && errorRight < 600 && errorRight != 0) { // Check for a line on the right side (Q3)
                     // Turn 90 degrees right
                     setMotors("90 right");
 
